0767c91768feae8b4525cecea686d6f06f0b52da
dev10castro <dcassor814@iescarrillo.es>
2024-11-18 20:01:24 +0100

    cambio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import path from 'path';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SigninComponent } from './components/auth/signin/signin.component';
+import { TasklistComponent } from './components/task/tasklist/tasklist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {path:'login',component:LoginComponent},
+   {path:'sigin',component:SigninComponent},
+   {path:'tasklist',component:TasklistComponent},
+   {path:'',redirectTo:'/tasklist',pathMatch:'full'}
+];
