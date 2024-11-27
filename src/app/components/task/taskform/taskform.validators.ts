c0d40e066d9172b96138ef951c9cf9b73da14383
dev10castro <dcassor814@iescarrillo.es>
2024-11-27 18:24:27 +0100

    parte 1 ejercicio 6

---
+++ src/app/components/task/taskform/taskform.validators.ts
@@ -0,0 +1,30 @@
+//login.validator.ts
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+//Ejemplo estructura función
+export function customValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    let valorCampo = control.value
+    let expireDate = new Date(valorCampo);
+    let today = new Date();
+
+    if(expireDate >= today){
+        return null;
+    }else{
+        return  { invalidDate: true };
+    }
+
+  };
+}
+
+export function customvalidatorPriority(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    let valorCampo:string = control.value
+
+    if(valorCampo == 'H' || valorCampo == 'M' || valorCampo == 'L'){
+        return null;
+    }else{
+        return  { invalidPriority: true};
+    }
+  };
+}
